0f8d9e0593618a41245ee16c67ca0e4dacd2b11d
Md Mustakin Ebne Mazid <mustakinshvn@gmail.com>
2024-05-25 03:50:04 +0600

    Create Connection object

--- Server.java
+++ Server.java
@@ -8,15 +8,21 @@ import java.util.Scanner;
 public class Server {
     public static void main(String[] args) {
 
-        final ServerSocket serverSocket;
-        final Socket clientSocket;
+        final ServerSocket serverSocket;   //creating a server that can accept and handle client requests over a network.
+
+        final Socket clientSocket;      //Sockets enable applications to send and receive data over a network, such as the internet.
+
         final BufferedReader in;
         final PrintWriter out;
         final Scanner sc= new Scanner(System.in);
 
 
         try{
-            serverSocket = new ServerSocket(5000);
+            
+            serverSocket = new ServerSocket(5000);  // Create a ServerSocket bound to port 5000
+
+            clientSocket = serverSocket.accept();   //It blocks the execution of the program until a client connects.
+            
     
         }catch(IOException e){
             e.printStackTrace();
@@ -24,7 +30,7 @@ public class Server {
         }
     }
 
-    
+
 
 
     }

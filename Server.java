9580857b3e58fd30a186161c7318e197d341e4e8
Md Mustakin Ebne Mazid <mustakinshvn@gmail.com>
2024-11-01 21:56:44 +0600

    Running

--- Server.java
+++ Server.java
@@ -1,86 +1,69 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.Vector;
 
-public class Server {
-    public static void main(String[] args) {
+public class Server implements Runnable {
+    
+      Socket socket;
 
-        final ServerSocket serverSocket;   //creating a server that can accept and handle client requests over a network.
+      public static Vector clients = new Vector();
 
-        final Socket clientSocket;      //Sockets enable applications to send and receive data over a network, such as the internet.
-
-        final BufferedReader in;   // used for reading text from an input stream efficiently
-
-        final PrintWriter out;  //used in the context of socket programming to send data to a server or a client.
-
-        final Scanner sc = new Scanner(System.in);
-
-
-        try{
-            
-            serverSocket = new ServerSocket(5000);  // Create a ServerSocket bound to port 5000
-
-            clientSocket = serverSocket.accept();   //It blocks the execution of the program until a client connects.
-
-            out = new PrintWriter(clientSocket.getOutputStream());  //responsible for sending data to the client.
-              
-             /*new InputStreamReader( clientSocket.getInputStream()) : creates a stream reader for the socket. However this stream reader only reads data as Bytes , therefore it must be passed to BufferedReader to be converted into characters. */
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  //use to read from clientSocket.
-
-   
-        //Sender thread : it contains the code the server will use to send messages to client
-         Thread sender = new Thread(new Runnable() {
-           String msg;
-
-        @Override
-        public void run() {
-            while (true) {
-                msg= sc.nextLine();
-                out.println(msg);
-                out.flush();  //used to clear any buffered data in an output stream, ensuring that all data is actually written out to the destination.
-                    }
-             } 
-         });
-
-
-         sender.start();
-
-
-        //Recieve thread : it contains the code the server will use to recieve messages from client
-
-         Thread receiver = new Thread(new Runnable() {
-            String msg;
-
-            @Override
-            public void run(){
-                try{
-                    msg= sc.nextLine();
-                while(msg!=null){
-                    System.out.println("Server : " +msg);
-                    msg= in.readLine();
-                }
-                System.out.println("Server is out of Service");
-                out.close();
-                clientSocket.close();
-                }catch(IOException e){
-                    e.printStackTrace();
-                }
-
-            }
-         });
-
-
-         receiver.start();
-   
-        }catch(IOException e){
+      public Server (Socket socket){
+        try {
+            this.socket=socket;
+        } catch (Exception e) {
             e.printStackTrace();
-            
+        }
+      }
+
+      public void run(){
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            clients.add(writer);
+              
+            while (true) {
+                String msg= reader.readLine().trim();
+                System.out.println("Received message : "+ msg);
+
+                for(int i=0; i<clients.size(); i++){
+                    BufferedWriter bw = (BufferedWriter) clients.get(i);
+                    bw.write(msg);
+                    bw.write("\r\n");
+                    bw.flush();
+                }
+            }
+
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+      }
+
+
+      
+
+    public static void main(String[] args) {
+     
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000); // Create a ServerSocket bound to port 5000
+          
+            System.out.println("Server is running and waiting for client connections on port 5000...");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept(); // It blocks the execution of the program until a client connects.
+              
+               Server server = new Server(clientSocket);    
+                
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    }
-
-  
+}
